b7048873c762e1ee24841248c552740976beaabb
Your Name <caihebatista@gmail.com>
2024-09-24 21:30:28 -0300

    Add tests for connecting to a SQLite database

---
+++ tests/connect.test.ts
@@ -0,0 +1,8 @@
+import { describe, test, expect } from 'bun:test'
+import { BunSqlight } from '../src/lib/connect'
+
+describe("Connection to SQLite DB", () => {
+    test.todo('Should be able to connect to a database')
+    test.todo('Should be able to disconnect from a database')
+    test.todo('Should be able to run a query')
+})
