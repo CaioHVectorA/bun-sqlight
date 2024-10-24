531762236b09747a9a807dca5815e4f2aff0eaa4
Your Name <caihebatista@gmail.com>
2024-10-23 21:11:10 -0300

    Refactor query builder to import MetricTimer utility class

--- src/lib/db-manager.ts
+++ src/lib/db-manager.ts
@@ -1,7 +1,8 @@
 import { Database } from 'bun:sqlite';
 import { QueryBuilder } from './query-builder';
+import { MetricTimer } from '../utils/metric-timer';
 
-function createDatabaseManager(builder: QueryBuilder, db: Database) {
+export function createDatabaseManager(builder: QueryBuilder, db: Database) {
     const manager = {} as any;
 
     // Copiar dinamicamente todos os métodos do QueryBuilder para o manager
@@ -14,7 +15,7 @@ function createDatabaseManager(builder: QueryBuilder, db: Database) {
             // Se o método for 'run', executa a query no banco de dados
             // console.log({ method })
             if (method.toUpperCase().includes('TABLE')) {
-                console.log('Rodou aqui! table')
+                // console.log('Rodou aqui! table')
                 // console.log({ builderResult: builderResult.run() })
                 // builder.actualQuery = []
                 db.exec(builderResult.run()); // Executa a query no banco
@@ -22,10 +23,9 @@ function createDatabaseManager(builder: QueryBuilder, db: Database) {
                 return;
             }
             if (method === 'run') {
-                console.log('Rodou aqui! run')
-                console.log({ builderResult })
+                // console.log('Rodou aqui! run')
                 const query = builder.run(); // Obter a query final
-                console.log('Executing query:', {query, builderResult});
+                // console.log('Executing query:', {query, builderResult});
                 return db.query(builderResult).all(); // Executa a query no banco
             }
 
@@ -35,14 +35,3 @@ function createDatabaseManager(builder: QueryBuilder, db: Database) {
 
     return manager;
 }
-
-const manager = (createDatabaseManager(new QueryBuilder(), new Database('f.db'))) as QueryBuilder
-manager.createTable('users', (table) => {
-    table.id()
-    table.string('name', { default: 'John Doe', unique: true, nullable: false })
-    table.integer('age', { default: 18, unique: false, nullable: false })
-})
-manager.insert('users', { id: 2, name: "Teste", age: 40 }).run()
-// CREATE TABLE users (id INTEGER, name TEXT)
-// queries
-// manager.select('*').from('users').where('id', 1).run();
