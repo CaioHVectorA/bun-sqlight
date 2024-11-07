ee0494ec475997a341dc0799b04680a26274cf8d
CaioHVectorA <caihebatista@gmail.com>
2024-11-06 21:42:09 -0300

    Tests: add more tests with db managment on schema

--- tests/schema.test.ts
+++ tests/schema.test.ts
@@ -1,6 +1,9 @@
 import { describe, test, expect } from 'bun:test'
 import { QueryBuilder } from '../src/lib/query-builder'
+import { DatabaseManager } from '../src/lib/db-manager'
+import { Database } from 'bun:sqlite'
 const querybuilder = new QueryBuilder()
+const db = new DatabaseManager(querybuilder, new Database(':memory:'))
 describe("Tables and schema", () => {
     test('Should be able to create a table', () => {
         const queryExpected = 'CREATE TABLE users (id INT, name VARCHAR(255))'
@@ -43,8 +46,48 @@ describe("Tables and schema", () => {
         })
         expect(query.run()).toBe(queryExpected)
     })
-    test.todo('Should be able to create a table with foreign key')
-    test.todo('Should be able to create a table with timestamps')
-    test.todo('Should be able to create a table with uuid')
+    test('Should be able to create a table with foreign key', () => {
+        const queryExpected = 'CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(id))'
+        const queryWithoutConstraints = querybuilder.createTable('users', (table) => {
+            table.id()
+            table.string('name')
+            table.foreign('user_id', 'users.id')
+        })
+        expect(queryWithoutConstraints.run()).toBe(queryExpected)
+        const queryWithCascadeExpected = 'CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE)'
+        const queryWithCascade = querybuilder.createTable('users', (table) => {
+            table.id()
+            table.string('name')
+            table.foreign('user_id', 'users.id', { onDelete: 'CASCADE' })
+        })
+        expect(queryWithCascade.run()).toBe(queryWithCascadeExpected)
+    })
+    test('Should be able to create a table with timestamps', () => {
+        const queryExpected = 'CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'
+        const query = querybuilder.createTable('users', (table) => {
+            table.id()
+            table.string('name')
+            table.timestamps()
+        })
+        expect(query.run()).toBe(queryExpected)
+    })
+    test('Should be able to create a table with uuid', () => {
+        const queryExpected = 'CREATE TABLE users (id UUID PRIMARY KEY, name TEXT NOT NULL)'
+        const query = querybuilder.createTable('users', (table) => {
+            table.uuid('id')
+            table.string('name')
+        })
+        expect(query.run()).toBe(queryExpected)
+    })
+    test.todo('Should do relationships with tables')
+    // With db managment
+    // Example: Products and Users
+    test.todo('Should be able to create and a row can be inserted')
     test.todo('Should be able to create a table with timestamps and updated_at should be updated automatically')
+    test.todo('Should be able to create with UUID and id should be generated automatically and unique')
+    test.todo('Should be able to create a table with autoincrement id and id should be generated automatically and unique')
+    test.todo('Should be able to create a table with a foreign key and can do a select with join')
+    test.todo('Should be able to create a table with a foreign key and cascade on delete and delete all related rows')
+    test.todo('Should be able to create a table with a foreign key and cascade on update and update all related rows')
+    // 
 })
