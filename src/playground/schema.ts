2b466f6d9c1f4da5c8f616c11f516d6c3d31dcff
CaioHVectorA <caihebatista@gmail.com>
2025-03-01 16:57:35 -0300

    Feat: more exampels

--- src/playground/schema.ts
+++ src/playground/schema.ts
@@ -1,17 +1,51 @@
-import { QueryBuilder } from "../lib/query-builder";
+import { DatabaseManager } from '../lib/db-manager';
+import { serve } from 'bun';
+import { QueryBuilder } from '../lib/query-builder';
+const qb = new QueryBuilder();
+const db = new DatabaseManager(qb, DatabaseManager.getDb('f.db')) as unknown as QueryBuilder;
+db.createTable('user', (table) => {
+  table.string('name');
+  table.integer('age');
+  table.id();
+  // table.uuid('uuid')
+});
+db.createTable('product', (table) => {
+  table.foreign('user_id', 'user.id');
+  table.id();
+  table.float('price');
+  table.string('name');
+});
 
-const qb = new QueryBuilder()
-qb.createTable('users', (table) => {
-    table.string('name')
-    table.integer('age')
-    table.id()
-    table.uuid('uuid')
-    table.float('integer')
-})
-console.log(qb.run())
-qb.createTable('product', (table) => {
-    table.foreign('user_id', 'users.id')
-    table.id()
-    table.float('price')
-})
-console.log(qb.run())
+db.insert('user', { name: 'John Doe', age: 18 }).run();
+db.insert('user', { name: 'Jane Doe', age: 20 }).run();
+db.insert('product', { user_id: 1, price: 100, name: 'Casca de banana' }).run();
+db.insert('product', { user_id: 2, price: 200, name: 'Olá' }).run();
+db.insert('product', { user_id: 2, price: 200, name: 'Olá2' }).run();
+db.insert('product', { user_id: 2, price: 200, name: 'Olá3' }).run();
+console.log(
+  db
+    .select('U.name, P.name as p_name')
+    .from('user')
+    .join('P.user_id', 'U.id', {
+      comparison: '!=',
+      type: 'RIGHT',
+      alias: {
+        product: 'P',
+        user: 'U',
+      },
+    })
+    .run()
+);
+console.log({
+  parts: qb
+    .select('U.name, P.name as p_name')
+    .from('user')
+    .join('P.user_id', 'U.id', {
+      comparison: '!=',
+      type: 'RIGHT',
+      alias: {
+        product: 'P',
+        user: 'U',
+      },
+    }).actualQuery,
+});
