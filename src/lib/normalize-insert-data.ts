db276ed9bc77ac70b70fb9db3aa2557f57014631
CaioHVectorA <caihebatista@gmail.com>
2025-03-02 18:59:41 -0300

    Feat: add normalizeInsertData function to format insert data for database

---
+++ src/lib/normalize-insert-data.ts
@@ -0,0 +1,24 @@
+import type { Tables } from './table';
+
+// add quotes to string values, convert boolean to 1 or 0, and convert Date to ISO string
+export const normalizeInsertData = (t: Tables, table: string, keys: string[]) => (value: any, index: number) => {
+  if (typeof value === 'string') {
+    return `"${value}"`;
+  }
+  if (typeof value === 'boolean') {
+    return value ? 1 : 0;
+  }
+  // the library should handle the Date conversion in different ways
+  if (value instanceof Date) {
+    const actualTable = t[table];
+    const field = actualTable[keys[index]];
+    if (field === 'DATE') {
+      return `"${value.toISOString().split('T')[0]}"`; // -> "2021-01-01"
+    }
+    if (field === 'DATETIME' || field === 'TIMESTAMP') {
+      return `"${value.toISOString().replace('T', ' ').replace('Z', '')}"`; // -> "2021-01-01 00:00:00"
+    }
+    return `"${value.toISOString()}"`; // -> "2021-01-01T00:00:00.000Z"
+  }
+  return value;
+};
