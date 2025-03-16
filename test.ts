b4ee0afc21c4f04864e96696344ca555735a1a83
CaioHVectorA <caihebatista@gmail.com>
2025-03-15 23:45:39 -0300

    Feat: add tsconfig.types.json for improved type declaration management; update type-generator and test files for better structure and functionality

--- test.ts
+++ test.ts
@@ -1,16 +1,21 @@
-import { Sqlight } from './dist/';
+import { Sqlight } from './dist/src';
 const db = new Sqlight();
 db.createTable('users', {
   id: 'INTEGER PRIMARY KEY',
   name: 'TEXT',
   age: 'INTEGER',
 });
-
+db.createTable('products', (q) => {
+  q.id();
+  q.string('name');
+  q.float('price');
+  q.boolean('available');
+});
 for (let i = 0; i < 10; i++) {
   db.insert('users', {
     name: 'user' + i,
     age: i,
+    id: i,
   }).run();
 }
-
-console.log(db.select('*').from('users').where('age', 5).run());
+// console.log(db.select('*').from('users').where('age', '>', 5).run());
